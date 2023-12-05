d62b61669c59c966498273f266878c5b35d69c5b
Anders Helgeland Vandvik <anders.vandvik@RSE-FVFFM2C8Q05N.internal.visma.com>
2023-12-05 08:35:42 +0100

    Cleanup in upload data functionality

--- api.py
+++ api.py
@@ -6,9 +6,11 @@ from typing import Tuple
 import requests
 from dotenv import find_dotenv, load_dotenv
 
-load_dotenv(find_dotenv())
 from generate_data import generate_data
 
+load_dotenv(find_dotenv())
+
+# Environment variables
 VISMA_CONNECT_CLIENT_ID = os.getenv("VISMA_CONNECT_CLIENT_ID")
 VISMA_CONNECT_CLIENT_SECRET = os.getenv("VISMA_CONNECT_CLIENT_SECRET")
 VISMA_CONNECT_GRANT_TYPE = os.getenv("VISMA_CONNECT_GRANT_TYPE")
@@ -16,8 +18,77 @@ VISMA_CONNECT_SCOPE = os.getenv("VISMA_CONNECT_SCOPE")
 VISMA_CONNECT_URL = os.getenv("VISMA_CONNECT_URL")
 IO_BASE_URL = os.getenv("IO_BASE_URL")
 
+# Constants
+MAX_RETRIES = 5
+SLEEP_DURATION = 3
 
-def update_access_token():
+
+def upload_data(tenant_id: str):
+    url, job_id = get_presigned_url(tenant_id)
+    if not url:
+        raise Exception("Error getting presigned url")
+    headers = _get_headers(include_token=False, tenant_id=tenant_id)
+    data = generate_data()
+    res = requests.put(
+        url,
+        data=json.dumps(data),  # NB! Need to use json.dumps() on the dict here
+        headers=headers,
+    )
+    _update_access_token(res.status_code)
+    sleep(SLEEP_DURATION)
+    res = get_job_status(tenant_id, job_id)
+    if res and res.get("status") == "success":
+        print("Data uploaded successfully")
+    else:
+        print(f"Error uploading data. Response from the status endpoint:\n{res}")
+
+
+def get_presigned_url(tenant_id: str) -> Tuple[str, str]:
+    url = f"{IO_BASE_URL}/presigned_url"
+    headers = _get_headers(tenant_id=tenant_id)
+    res = requests.get(url, headers=headers)
+    _update_access_token(res.status_code)
+    if res.status_code == 200:
+        return res.json().get("url"), res.json().get("jobId")
+    else:
+        print(f"Error getting presigned url. Response:\n{res.json()}")
+        return None
+
+
+def get_job_status(tenant_id: str, job_id: str) -> str:
+    url = f"{IO_BASE_URL}/status"
+    headers = _get_headers(tenant_id=tenant_id, job_id=job_id)
+    response = requests.get(url, headers=headers)
+
+    tries = 0
+    while response.status_code == 404 and tries < MAX_RETRIES:
+        tries += 1
+        sleep(SLEEP_DURATION)
+        response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        while response.json().get("status") == "inProgress":
+            sleep(SLEEP_DURATION)
+            response = requests.get(url, headers=headers)
+        return response.json()
+    else:
+        return None
+
+
+def _get_headers(include_token: bool = True, tenant_id: str = "", job_id: str = ""):
+    headers = {}
+    if include_token:
+        headers["Authorization"] = f'Bearer {os.getenv("ACCESS_TOKEN")}'
+    if tenant_id:
+        headers["tenantId"] = tenant_id
+    if job_id:
+        headers["jobId"] = job_id
+    return headers
+
+
+def _update_access_token(status_code: int):
+    if status_code != 403:
+        return
     res = requests.post(
         url=VISMA_CONNECT_URL,
         data={
@@ -28,6 +99,11 @@ def update_access_token():
         },
     ).json()
     access_token = res["access_token"]
+    _update_env_file("ACCESS_TOKEN", access_token)
+    raise Exception("Access token had expired. New token generated. Try again.")
+
+
+def _update_env_file(access_token: str):
     with open(".env", "r") as f:
         lines = f.readlines()
     for i, line in enumerate(lines):
@@ -40,57 +116,4 @@ def update_access_token():
         f.writelines(lines)
 
 
-def get_presigned_url() -> Tuple[str, str]:
-    url = f"{IO_BASE_URL}/presigned_url"
-    headers = {
-        "Authorization": f'Bearer {os.getenv("ACCESS_TOKEN")}',
-        "tenantId": os.getenv("TENANT_ID"),
-    }
-    res = requests.get(url, headers=headers)
-    if res.status_code == 200:
-        return res.json().get("url"), res.json().get("jobId")
-    else:
-        print(f"Error getting presigned url. Response:\n{res.json()}")
-        return None
-
-
-def upload_data():
-    url, job_id = get_presigned_url()
-    if url is not None:
-        headers = {"tenantId": os.getenv("TENANT_ID")}
-        data = generate_data()
-        res = requests.put(
-            url,
-            data=json.dumps(data),  # NB! Need to use json.dumps() on the dict here
-            headers=headers,
-        )
-        sleep(3)
-        res = get_job_status(os.getenv("TENANT_ID"), job_id)
-        print(res)
-
-
-def get_job_status(tenant_id, job_id, max_retries=5) -> str:
-    url = f"{IO_BASE_URL}/status"
-    headers = {
-        "Authorization": f'Bearer {os.getenv("ACCESS_TOKEN")}',
-        "tenantId": tenant_id,
-        "jobId": job_id,
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 404:
-        tries = 0
-        for _ in range(max_retries):
-            tries += 1
-            sleep(3)
-            response = requests.get(url, headers=headers)
-            if response.status_code != 404:
-                break
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
-
-
-# update_access_token()
-# get_presigned_url()
-upload_data()
+upload_data(os.getenv("TENANT_ID"))
